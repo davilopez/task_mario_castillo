acc4ba2438eae7412b2455a17dee5f8daf8f002b
Mario Castillo <mariocg89@gmail.com>
2020-11-02 04:08:21 +0100

    Task 1 completed. Task 2 completed. Both tests are included within App.test.js file.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,113 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { act } from "react-dom/test-utils";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+import App from './App';
+import Table from './components/Table';
+import tableData from './data'
+
+
+let container;
+
+beforeEach(() => {
+  container = document.createElement('div');
+
+  container.setAttribute("id", "root");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  document.body.removeChild(container);
+  container = null;
+});
+
+describe('Task 1', () => {
+  it('Renders in table rows based on provided columns.', () => {
+    // Render App component
+    act(() => {
+      ReactDOM.render(<App />, container);
+    });
+    // There should be only 1 table element
+    const table = container.querySelector('table');
+    expect(typeof(table)).toEqual("object");
+
+    const thead = table.tHead;
+    // The number of th tags should be equal to number of columns
+    expect(thead.rows[0].cells.length).toEqual(tableData.columns.length);
+
+    // The table should have exact amount of rows
+    const tbody = table.getElementsByTagName("tbody")[0];
+    const trows = tbody.getElementsByTagName("tr");
+    expect(trows.length).toEqual(tableData.rows.length);
+
+  });
+});
+
+describe('Task 2', () => {
+  it('Clicking on headers sort table data.', () => {
+    // Render App component
+    act(() => {
+      ReactDOM.render(<App />, container);
+    });
+    // There should be only 1 table element
+    let table = container.querySelector('table');
+
+    const thead = table.tHead;
+    thead.rows[0].cells[1];
+    const movieHeader = thead.rows[0].cells[1];
+    expect(movieHeader.innerHTML.trim()).toBe("Movie");
+
+    let tbody = table.getElementsByTagName("tbody")[0];
+    let trows = tbody.getElementsByTagName("tr")[0]; // Get first row
+    let tcell = tbody.getElementsByTagName("td")[1]; // Get Movie column
+    expect(tcell.innerHTML.trim()).toEqual("Iron Man"); // First movie name in the list is Iron Man
+
+    // Perform click on "Movie" header
+    act(() => {
+      movieHeader.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+    });
+
+    table = container.querySelector('table');
+    // The table should have exact amount of rows
+    tbody = table.getElementsByTagName("tbody")[0];
+    trows = tbody.getElementsByTagName("tr")[0]; // Get first row
+    tcell = tbody.getElementsByTagName("td")[1]; // Get Movie column
+    expect(tcell.innerHTML.trim()).toEqual("Ant-Man"); // First movie name after sorting by name is Ant-Man
+
+  });
+});
+
+
+describe('Task 3', () => {
+  it('Fill Movie filter field to filter the table data.', () => {
+    // Render App component
+    act(() => {
+      ReactDOM.render(<App />, container);
+    });
+    // There should be only 1 table element
+    let table = container.querySelector('table');
+
+    const thead = table.tHead;
+    thead.rows[0].cells[1];
+    const movieHeader = thead.rows[0].cells[1];
+    expect(movieHeader.innerHTML.trim()).toBe("Movie");
+
+    let tbody = table.getElementsByTagName("tbody")[0];
+    let trows = tbody.getElementsByTagName("tr")[0]; // Get first row
+    let tcell = tbody.getElementsByTagName("td")[1]; // Get Movie column
+    expect(tcell.innerHTML.trim()).toEqual("Iron Man"); // First movie name in the list is Iron Man
+
+    // Perform click on "Movie" header
+    act(() => {
+      movieHeader.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+    });
+
+    table = container.querySelector('table');
+    // The table should have exact amount of rows
+    tbody = table.getElementsByTagName("tbody")[0];
+    trows = tbody.getElementsByTagName("tr")[0]; // Get first row
+    tcell = tbody.getElementsByTagName("td")[1]; // Get Movie column
+    expect(tcell.innerHTML.trim()).toEqual("Ant-Man"); // First movie name after sorting by name is Ant-Man
+
+  });
 });
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,22 +1,178 @@
-import React from "react"
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import AscLogo from './../resources/order-ascending.svg'
+import DescLogo from './../resources/order-descending.svg'
 
-const Table = () => (
-    <table>
-        <thead>
-            <tr>
-                <th>First column</th>
-                <th>Second column</th>
-                <th>Third column</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>One</td>
-                <td>Two</td>
-                <td>Three</td>
-            </tr>
-        </tbody>
-    </table>
-)
+
+class Table extends Component {
+  constructor(props) {
+    super(props);
+    console.debug("Building table.");
+    this.data = props.tableData;
+    this.sortColumn = props.orderBy;
+  }
+
+  componentWillMount(){
+    this.renameHeaders();
+  }
+
+  sortByColumn(orderColumn){
+      console.debug("Column " + orderColumn.id + " sorted.");
+      if(typeof(this.sortColumn) ===  'undefined'){
+        // First click on any header
+        this.sortColumn = orderColumn;
+      }else if(this.sortColumn.index !== orderColumn.index){
+        // First click on other header
+        this.sortColumn.orderAsc = true;
+        this.sortColumn = orderColumn;
+      }else{
+        // Repeated click on header
+        orderColumn.orderAsc = !orderColumn.orderAsc;
+
+      }
+      this.sortData(this.data,this.sortColumn);
+      ReactDOM.render(<Table tableData={this.data} orderBy={this.sortColumn} filter={[]} />, document.getElementById('root'))
+  }
+
+  sortData(dataBefore,orderColumn){
+
+    if(orderColumn.orderAsc){
+      // Ascending order
+      if(orderColumn.index !== 2){
+        // Sorting method for numbers and strings leaving undefined values at the end
+        dataBefore.rows.sort(function (a, b) {
+          if(typeof a[orderColumn.id] === 'undefined')
+            return -1;
+          if (a[orderColumn.id] > b[orderColumn.id]) {
+            return 1;
+          }
+          if (a[orderColumn.id] < b[orderColumn.id]) {
+            return -1;
+          }
+          return 0;
+        });
+      }else {
+        console.log("order date")
+        // Sorting Release Date column
+        dataBefore.rows.sort(function (a, b) {
+          if(typeof a[orderColumn.id] === 'undefined')
+            return -1;
+          if(typeof b[orderColumn.id] === 'undefined')
+            return 1;
+
+          if(a[orderColumn.id] === 'Unknown'){
+            return -1;
+          }
+          if(b[orderColumn.id] === 'Unknown') {
+            return 1;
+          }
+
+          let dateA = a[orderColumn.id];
+          let dateB = b[orderColumn.id];
+          console.log("a");
+          console.log(a);
+          console.log("b");
+          console.log(b);
+          // Create Date object based on A and B strings when possible
+          if(a[orderColumn.id].match( /\d{2}-\d{2}-\d{4}/ )){
+            dateA = new Date(a[orderColumn.id].replace( /(\d{2})-(\d{2})-(\d{4})/, "$2/$1/$3"));
+          }
+
+          if(b[orderColumn.id].match( /\d{2}-\d{2}-\d{4}/ )){
+            dateB = new Date(b[orderColumn.id].replace( /(\d{2})-(\d{2})-(\d{4})/, "$2/$1/$3"));
+          }
+
+          if(typeof(dateA) !== 'undefined' && typeof(dateB) !== 'undefined'){
+            if (dateA > dateB) {
+              return 1;
+            }
+            if (dateA < dateB) {
+              return -1;
+            }
+            return 0;
+          }
+        });
+
+      }
+
+    }else{
+      // Descending order, the rows array will be reversed
+      dataBefore.rows.reverse();
+    }
+    return dataBefore;
+  }
+
+  renameHeaders(){
+    console.debug("Renaming header.");
+    // This method will be executed when each header is clicked
+    this.headers = this.data.columns.map((eachHeader,headerIndex)=>{
+      if(eachHeader.id === "number"){
+        eachHeader.title = "#"
+      }
+      if(eachHeader.id === "productionBudget" || eachHeader.id === "worldwideBoxOffice"){
+          if(eachHeader.title.indexOf('(') === -1){
+            let customTitle = eachHeader.title.slice(0, eachHeader.title.length-4) + "(" +eachHeader.title.slice( eachHeader.title.length-4) + ")" ;
+            eachHeader.title = customTitle;
+          }
+      }
+      eachHeader.index = headerIndex;
+      eachHeader.orderAsc = true;
+      return eachHeader;
+    });
+  }
+
+  render() {
+    console.debug("Rendering table.");
+    const headersTags = this.headers.map((eachHeader)=>{
+      let title = eachHeader.title;
+      let orderIcon;
+
+      if(typeof(this.sortColumn) !== 'undefined' && this.sortColumn.id === eachHeader.id){
+      console.debug(" Order by " + this.sortColumn.id);
+        if(this.sortColumn.orderAsc){
+          orderIcon = <img src={AscLogo} alt="Ascending Order" />;
+        }else{
+          orderIcon = <img src={DescLogo} alt="Descending Order" />;
+        }
+      }
+      return(
+        <th key={eachHeader.id} id={eachHeader.id} onClick={() => this.sortByColumn(eachHeader)}>{title} {orderIcon}</th>
+      )
+    });
+
+    const rows = this.data.rows.map((eachRow,index) =>
+      <tr key={index}>
+        {this.headers.map((eachHeader,headerIndex)=>{
+          let rowContent = eachRow[eachHeader.id];
+          let amountCell = '';
+          if(headerIndex === 2){ // This equals 2 is to find the 'release date' column
+            rowContent = (eachRow[eachHeader.id].match( /\d{2}-\d{2}-\d{4}/ ))?eachRow[eachHeader.id].replace( /(\d{2})-(\d{2})-(\d{4})/, "$2/$1/$3"):eachRow[eachHeader.id];
+          }if(headerIndex > 2){ // This greater than 2 is to find last two columns which will be formatted and will be aligned to the right through the class amountCell
+            rowContent = (typeof eachRow[eachHeader.id] === 'number')?new Intl.NumberFormat("en-EN").format(eachRow[eachHeader.id]):0;
+            amountCell = ' amountCell';
+          }
+          return(
+            <td key={headerIndex + '-' + index} className={'eachCell' + amountCell}>{rowContent}</td>
+          )
+        })}
+      </tr>
+    );
+
+    return (
+      <table key="TableKey">
+          <thead>
+              <tr>
+                {headersTags}
+              </tr>
+          </thead>
+          <tbody>
+              {rows}
+          </tbody>
+      </table>
+    )
+  }
+}
+
+
 
 export default Table
